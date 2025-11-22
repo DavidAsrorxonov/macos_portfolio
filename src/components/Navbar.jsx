071df2665cb7703916be2521f4832c3640d0b0fa
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-22 20:12:20 +0900

    feat: add basic Navbar structure with logo and portfolio title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <main>
-            Navbar
-        </main>
+        <nav>
+            <div>
+                <img src='/images/logo.svg' alt='logo' />
+                <p className='font-bold'>Dovudkhon's Portfolio</p>
+
+                <ul></ul>
+            </div>
+        </nav>
     )
 }
 
