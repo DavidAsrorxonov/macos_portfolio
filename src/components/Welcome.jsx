e5d8a358697fe1618324d4f34c7e525190c32518
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-23 15:12:48 +0900

    feat: implement a structured welcome section with title, subtitle, refs, and a small-screen warning.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Welcome = () => {
+    const titleRef = useRef(null)
+    const subtitleRef = useRef(null)
+
     return (
-        <div>Welcome</div>
+        <section id='welcome'>
+            <p ref={subtitleRef}>Hey, I'm Dovudkhon! Welcome to my</p>
+            <h1 ref={titleRef} className='mt-7'>portfolio</h1>
+
+            <div className="small-screen">
+                <p>This portfolio is designed for desktop/tablet screens only.</p>
+            </div>
+        </section>
     )
 }
 
