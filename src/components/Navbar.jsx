e8e24ea80702faa54eaad7a417655d042b5799a0
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-23 10:02:21 +0900

    feat: import and render `navIcons` in a new navigation section.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { links } from '#constants'
+import { links, navIcons } from '#constants'
 import React from 'react'
 
 const Navbar = () => {
@@ -12,6 +12,15 @@ const Navbar = () => {
                     {links.map(({ id, name }) => (
                         <li key={id}>
                             <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img src={img} className='icon-hover' alt={`icon-${id}`} />
                         </li>
                     ))}
                 </ul>
