47d6823056e22d8f0b032c92a66b98691ba59f9c
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-24 15:44:29 +0900

    feat: introduce Immer and window configuration constants for state management

--- src/store/window.js
+++ src/store/window.js
@@ -1,1 +1,7 @@
+import { WINDOW_CONFIG } from "#constants/windowConfig";
 import { create } from "zustand";
+import {immer} from "zustand/middleware/immer"
+
+const useWindowStore = create(immer((set) => {
+    windows: WINDOW_CONFIG
+}))
