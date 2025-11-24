ccc9ba4f800482379be106877dd17bef3f242fe2
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-24 16:42:34 +0900

    feat: Implement `WindowWrapper` HOC to wrap components with a section element, applying z-index, a ref, and setting a display name.

--- src/hoc/windowWrapper.jsx
+++ src/hoc/windowWrapper.jsx
@@ -1,11 +1,24 @@
 import useWindowStore from "#store/window";
-import React from "react";
+import React, { useRef } from "react";
 
 const WindowWrapper = (Component, windowKey) => {
   const Wrapped = (props) => {
     const { focusWindow, windows } = useWindowStore();
+    const { isOpen, zIndex } = windows[windowKey];
+    const ref = useRef(null);
+
+    return (
+      <section id={windowKey} ref={ref} style={{ zIndex }} className="absolute">
+        <Component {...props} />
+      </section>
+    );
   };
-  return <div>windowWrapper</div>;
+
+  Wrapped.displayName = `WindowWrapper(${
+    Component.displayName || Component.name || "Component"
+  })`;
+
+  return Wrapped;
 };
 
 export default WindowWrapper;
