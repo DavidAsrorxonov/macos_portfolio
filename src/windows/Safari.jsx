68af5b06c61398584e009ede546ef436476eb229
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-24 23:58:22 +0900

    feat: add new icons and a search bar to the Safari component UI

--- src/windows/Safari.jsx
+++ src/windows/Safari.jsx
@@ -1,6 +1,15 @@
 import { WindowControls } from "#components";
 import WindowWrapper from "#hoc/windowWrapper";
-import { ChevronLeft, ChevronRight, PanelLeft } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Copy,
+  PanelLeft,
+  Plus,
+  Search,
+  Share,
+  ShieldHalf,
+} from "lucide-react";
 import React from "react";
 
 const Safari = () => {
@@ -13,6 +22,25 @@ const Safari = () => {
         <div className="flex items-center gap-1 ml-5">
           <ChevronLeft className="icon" />
           <ChevronRight className="icon" />
+        </div>
+
+        <div className="flex-1 flex-center gap-3">
+          <ShieldHalf className="icon" />
+
+          <div className="search">
+            <Search className="icon" />
+            <input
+              type="text"
+              placeholder="Search or enter website name"
+              className="flex-1"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <Share className="icon" />
+          <Plus className="icon" />
+          <Copy className="icon" />
         </div>
       </div>
     </>
