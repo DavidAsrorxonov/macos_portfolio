adeb06d31af325124de49e44e0085e1e7cc29166
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-25 15:40:54 +0900

    feat: Add react-pdf library and integrate it into the Resume component.

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -2,6 +2,14 @@ import { WindowControls } from "#components";
 import WindowWrapper from "#hoc/windowWrapper";
 import { Download } from "lucide-react";
 import React from "react";
+import { pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 const Resume = () => {
   return (
