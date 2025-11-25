2209bc956ab40321eb0d4043e3c57ce2cbb5636c
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-25 21:38:12 +0900

    refactor: update Finder's `renderItem` to encapsulate section titles and lists.

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -9,19 +9,25 @@ import React from "react";
 const Finder = () => {
   const { activeLocation, setActiveLocation } = useLocationStore();
 
-  const renderItem = (item) =>
-    item.map((item) => (
-      <li
-        key={item.id}
-        onClick={() => setActiveLocation(item)}
-        className={clsx(
-          item.id === activeLocation.id ? "active" : "not-active"
-        )}
-      >
-        <img src={item.icon} className="w-4" alt={item.name} />
-        <p className="text-sm font-medium truncate">{item.name}</p>
-      </li>
-    ));
+  const renderItem = (name, item) => (
+    <div>
+      <h3>{name}</h3>
+      <ul>
+        {item.map((item) => (
+          <li
+            key={item.id}
+            onClick={() => setActiveLocation(item)}
+            className={clsx(
+              item.id === activeLocation.id ? "active" : "not-active"
+            )}
+          >
+            <img src={item.icon} className="w-4" alt={item.name} />
+            <p className="text-sm font-medium truncate">{item.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 
   return (
     <>
@@ -32,15 +38,8 @@ const Finder = () => {
 
       <div className="bg-white flex h-full">
         <div className="sidebar">
-          <div>
-            <h3>Favourites</h3>
-            <ul>{renderItem(Object.values(locations))}</ul>
-          </div>
-
-          <div>
-            <h3>Work</h3>
-            <ul>{renderItem(locations.work.children)}</ul>
-          </div>
+          {renderItem("Favourites", Object.values(locations))}
+          {renderItem("Work", locations.work.children)}
         </div>
       </div>
     </>
