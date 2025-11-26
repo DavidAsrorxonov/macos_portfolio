7cc030e87b38d3038b793d34c6d004bfdda0fb23
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-27 00:28:15 +0900

    feat: Add Instagram and Telegram social icons and configuration, removing Atom and Twitter icons.

---
+++ src/constants/socials.js
@@ -0,0 +1,30 @@
+export const socials = [
+  {
+    id: 1,
+    text: "Github",
+    icon: "/icons/github.svg",
+    bg: "#f4656b",
+    link: "https://github.com/DavidAsrorxonov",
+  },
+  {
+    id: 2,
+    text: "LinkedIn",
+    icon: "/icons/linkedin.svg",
+    bg: "#05b6f6",
+    link: "https://www.linkedin.com/in/dovudkhon0211",
+  },
+  {
+    id: 3,
+    text: "Telegram",
+    icon: "/icons/telegram.svg",
+    bg: "#0088cc",
+    link: "https://t.me/whoisdave02",
+  },
+  {
+    id: 4,
+    text: "Instagram",
+    icon: "/icons/instagram-icon.svg",
+    bg: "#e1306c",
+    link: "https://www.instagram.com/adovudkhan",
+  },
+];
