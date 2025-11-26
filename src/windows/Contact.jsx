83ae898d98e370df031241e02b62804c3deab2ac
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-11-27 00:20:26 +0900

    feat: Implement initial Contact window UI with header, image, and introductory text, and integrate with WindowWrapper.

--- src/windows/Contact.jsx
+++ src/windows/Contact.jsx
@@ -1,7 +1,32 @@
+import WindowWrapper from "#hoc/windowWrapper";
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div id="window-header">
+        <h2>Contact me</h2>
+      </div>
+
+      <div className="p-5 space-y-5">
+        <img
+          src="/images/square-image.jpg"
+          alt="david"
+          className="w-60 rounded-full"
+        />
+
+        <h3>Let's Connect</h3>
+        <p>
+          I turn caffeine into code and complex problems into elegant solutions.
+          My superpower? Finding that one missing semicolon at 3 AM.
+        </p>
+
+        <ul></ul>
+      </div>
+    </>
+  );
 };
 
-export default Contact;
+const ContactWindow = WindowWrapper(Contact, "contact");
+
+export default ContactWindow;
